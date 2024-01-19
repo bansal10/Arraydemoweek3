c5cd918e0351a3d204fc32f4a9de1cec5706401b
Aman Aman <aman10@sheridancollege.ca>
2024-01-19 17:01:25 -0500

    Revert "third commit"
    This reverts commit 17aa88f1e7c4dfefd4f73a240efc2018bf60cfbc.

--- src/arraydemo/Student.java
+++ src/arraydemo/Student.java
@@ -1,10 +1,12 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package arraydemo;
 
 /**
  * This is to show modification
  * @author aman
- * this is show revert commit
  */
 public class Student {
     private int sID;
